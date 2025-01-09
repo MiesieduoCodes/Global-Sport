f152a74896fd74e8d11815eb77bf97867df26895
MiesieduoCodes <miesieduoveria@gmail.com>
2025-01-09 15:59:43 +0100

    here

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,12 +24,14 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
+            
             <ModeToggle />
             {children}
           </ThemeProvider>
